9470e63eb0d29df3fde176dbf7e93c6bedf29af9
tara-fenton <tara@itsovertime.com>
2020-08-31 20:44:25 -0400

    login google oauth

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var router = require("express").Router();
+const passport = require("passport");
+
+// The root route renders our only view
+router.get("/", function(req, res) {
+  res.render("index", { title: "Spotify v2.0", user: req.user });
+});
+
+// Google OAuth login route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// Google OAuth callback route
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/songs",
+    failureRedirect: "/",
+  })
+);
+
+// OAuth logout route
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
