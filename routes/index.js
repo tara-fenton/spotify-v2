c487e04c455c19c8cbc15d0d240c459026f09f1e
tara-fenton <tara@itsovertime.com>
2020-09-02 00:30:01 -0400

    nav partials

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get(
 router.get(
   "/oauth2callback",
   passport.authenticate("google", {
-    successRedirect: "/songs",
+    successRedirect: "/playlists",
     failureRedirect: "/",
   })
 );
